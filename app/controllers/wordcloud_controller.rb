9132b04d1b1eccbc038fcda0b8f412b82e2a5982
Michael Steger <mjsteger1@gmail.com>
2014-09-27 22:21:43 -0700

    Adding the ability to use d3, which is far faster, and various tweaks

--- app/controllers/wordcloud_controller.rb
+++ app/controllers/wordcloud_controller.rb
@@ -1,7 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
 TRANSCRIPT_URL = "http://www.springfieldspringfield.co.uk/"
-TO_KEEP = 60
+TO_KEEP = 150
+SIZE_FACTOR = 10
 class WordcloudController < ApplicationController
   def view
     return unless params[:tv_show]
@@ -24,17 +25,22 @@ class WordcloudController < ApplicationController
         join.
         encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '').
         gsub(/\r|\n|\[ [^\]]+ \]/x, "").
-        scan(/\b[\w|']+\b/)
+        scan(/\b[\w|']+\b/).
+        map(&:downcase)
       hash = Hash.new { |hash, key| hash[key] = 0 }
       text_array.each { |x| hash[x]+= 1 }
-      to_delete = %w(a the it of to and is this was I you me that on)
-      to_delete + ('A'..'Z').to_a
+      to_delete = %w(a the it of to and is this was I you me that on it's what that that's in for if by did my have so do be your I'm I'll her his can are an he as our at but she you're but or we not too has her him can am had i've well will let off we're we've he's)
+      to_delete = to_delete + ('A'..'Z').to_a
       to_delete.each do |delete|
         hash.delete(delete.capitalize)
+        hash.delete(delete)
         hash.delete(delete.downcase)
       end
-      # hash = Hash[hash.sort_by(&:last).last(TO_KEEP)]
-      @word_array = hash.map { |(key, val)| {text: key, weight: val} }
+      to_keep = params[:to_keep] || TO_KEEP
+      hash = Hash[hash.sort_by(&:last).last(to_keep)]
+      params[:visualization] ||= "instant"
+      value_key = params[:visualization] == "instant" ? :size : :weight
+      @word_array = hash.map { |(key, val)| {text: key, value_key => (val * 3) + SIZE_FACTOR} }
     end
   end
 end
